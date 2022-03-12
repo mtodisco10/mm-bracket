9522936b234c1785c40be3079a456b4aa2ba37c9
mtodisco10 <mtodisco10@gmail.com>
2022-03-12 15:57:57 -0500

    importing psycopg2

--- db_check.py
+++ db_check.py
@@ -2,13 +2,14 @@
 import psycopg2
 
 #con = sqlite3.connect('winners.db')
-conn = psycopg2.connect(
+con = psycopg2.connect(
    database="d50p3glj8p4gtu",
    user='nlavwjdwgcxeyg',
     password='0d2d99ddbae0886aec120f4239222d15aee55f2b2cf48f4a19d663c443fe6c49',
     host='ec2-54-158-26-89.compute-1.amazonaws.com',
     port= '5432'
 )
+
 cur = con.cursor()
 
 for row in cur.execute('select * from winners'):
