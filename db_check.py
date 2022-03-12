6ddb56532e262b19da04cd95c1e904bf919cd7a6
mtodisco10 <mtodisco10@gmail.com>
2022-03-12 15:50:16 -0500

    db check on server

--- db_check.py
+++ db_check.py
@@ -1,6 +1,16 @@
-import sqlite3
+#import sqlite3
+#import psycopg2
 
-con = sqlite3.connect('winners.db')
+#con = sqlite3.connect('winners.db')
+conn = psycopg2.connect(
+   database="d50p3glj8p4gtu",
+   user='nlavwjdwgcxeyg',
+    password='0d2d99ddbae0886aec120f4239222d15aee55f2b2cf48f4a19d663c443fe6c49',
+    host='ec2-54-158-26-89.compute-1.amazonaws.com',
+    port= '5432'
+)
+
+conn = psycopg2.connect("dbname= user= password=")
 cur = con.cursor()
 
 for row in cur.execute('select * from winners'):
